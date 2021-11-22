a40593099984f6d1cba010a200d821e2301ee899
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-11-22 23:37:25 +0530

    Improved WordBreak  Solution added

--- dsa/src/dsaProblems/Word_Break.java
+++ dsa/src/dsaProblems/Word_Break.java
@@ -1,10 +1,70 @@
 package dsaProblems;
+import  java.util.*;
+
+
 
 public class Word_Break {
-    public  static  void wordBreak(){
-        
+    public static int wordBreak(String a, String []  b ,int index )
+    {
+        //code here
+
+        if(index==a.length()) {
+            System.out.println("Inside break condition ");
+            return 1;
+        }
+
+
+        for(int i=0;i<b.length;i++)
+        {
+            String current=b[i];
+
+            if(a.charAt(index)==current.charAt(0))
+            {
+                System.out.println(  "First Character Matched ");
+
+                if(current.length()==1) {
+                    System.out.println(   "Input String : "+ a+"___ " +"Current: "+current);
+                    return wordBreak(a.substring(index + current.length()), b, index);
+
+
+
+
+                }                else
+                {
+                    if(a.substring(0,current.length()  ).equals(current))
+                    {System.out.println("current Input :" +a+"___"+ " Required String :"+a.substring(0,current.length() ) +"___"+" Current:"+current);
+//                        System.out.println(   "Input String : "+ a+" " +"Current: "+current);
+                     return    wordBreak(a.substring(index+current.length()),b,index);
+                    }
+                }
+
+            }
+            System.out.println(  "First Character Not  Matched ");
+            System.out.println(  "Input: "+a+"___ "+ " Current: "+current);
+
+        }
+
+
+
+        return 0;
+
     }
     public static void main(String[] args) {
+
+        String [] b = { "i", "like", "sam","sung", "samsung", "mobile",
+                        "ice","cream", "icecream","man", "go", "mango"
+        };
+
+        String a = "ilike";
+        int index=0;
+         int ans= wordBreak(a,b,0);
+         if(ans==0)
+             System.out.println("Not Possible to break The String");
+         else
+             System.out.println("Word is successfully Breaked ");
+
+
+
 
     }
 }
---
+++ dsa/src/dsaProblems/wod_break_Rec_two.java
@@ -0,0 +1,63 @@
+package dsaProblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class wod_break_Rec_two<dictionary> {
+    public  static  boolean word_break(String word ,String [] dictionary){
+        int length=word.length();
+        if(length==0)
+            return true;
+        for (int i=0; i<=length;i++)
+            if(cantains(word.substring(0,i),dictionary)
+                    &&
+                    word_break(word.substring(i,length),dictionary )) {
+//             System.out.println(word.substring(0,i));
+               return true;
+            }
+
+        return  false;
+
+    }
+
+    private static boolean cantains(String word, String [] dictionary ){
+        int size=word.length();
+        int dictionaryLength=dictionary.length;
+        for (int i=0;i<dictionaryLength;i++)
+            if(dictionary[i].equals(word))
+                return true;
+
+            return false ;
+
+    }
+    public static int wordBreak(String word, ArrayList<String> list  )
+    {
+        //code here
+        int length=word.length();
+        if(length==0)
+            return 1;
+
+        for(int i=1;i<=length;i++)
+            if(list.contains(word.substring(0,i)) && wordBreak(word.substring(i,length),list)==1)
+                return 1;
+
+        return 0 ;
+
+
+    }
+
+    public static void main(String[] args) {
+        String [] dictionary ={ "i", "like", "sam",
+                "sung", "samsung", "mobile",
+                "ice","cream", "icecream",
+                "man", "go", "mango" };
+
+        String str="ilike";
+        boolean output =word_break(str,dictionary);
+        System.out.println(output);
+        ArrayList<String> list=new ArrayList<>();
+        list.con
+
+
+    }
+}
