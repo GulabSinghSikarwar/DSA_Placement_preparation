6b3b9a182735dd3187c8f3dcb4cd969c425bbdce
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-12-02 00:12:17 +0530

    Boyer Moore Algorithm for Pattern Searching

--- dsa/src/dsaProblems/Bayer_more_algo.java
+++ dsa/src/dsaProblems/Bayer_more_algo.java
@@ -1,76 +1,52 @@
 package dsaProblems;
 
 public class Bayer_more_algo {
-    static int NO_OF_CHARS = 256;
-    //A utility function to get maximum of two integers
-    static int max (int a, int b) { return (a > b)? a: b; }
-    //The preprocessing function for Boyer Moore's
-//bad character heuristic
-    static void badCharHeuristic( char []str, int size,int badchar[])
+    static int no_of_characters=256;
+
+    public static  void  patternSearch(String text,String pat )
     {
-// Initialize all occurrences as -1
-        for (int i = 0; i < NO_OF_CHARS; i++)
-            badchar[i] = -1;
-// Fill the actual value of last occurrence
-// of a character (indices of table are ascii and values are index of
-//        occurence)
-        for (int i = 0; i < size; i++)
-            badchar[(int) str[i]] = i;
-    }
-    /* A pattern searching function that uses Bad
-    Character Heuristic of Boyer Moore Algorithm */
-    static void search( char txt[], char pat[])
-    {
-        int m = pat.length;
-        int n = txt.length;
-        int badchar[] = new int[NO_OF_CHARS];
-/* Fill the bad character array by calling
-the preprocessing function badCharHeuristic()
-for given pattern */
-        badCharHeuristic(pat, m, badchar);
-        int s = 0; // s is shift of the pattern with
-// respect to text
-//there are n-m+1 potential allignments
-        while(s <= (n - m))
+        int m=pat.length();
+        int n=text.length();
+
+        int bad_chars []=new int[no_of_characters];
+
+        bad_huristic(pat,m,bad_chars);
+        int shift=0;
+
+        while(shift<=(n-m))
         {
-            int j = m-1;
-/* Keep reducing index j of pattern while
-characters of pattern and text are
-matching at this shift s */
-            while(j >= 0 && pat[j] == txt[s+j])
+
+            int j=(m-1);
+            while (j>=0 && pat.charAt(j)==text.charAt(shift+j))
                 j--;
-/* If the pattern is present at current
-shift, then index j will become -1 after
-the above loop */
-            if (j < 0)
+            if (j<0)
             {
-                System.out.println("Patterns occur at shift = " + s);
-/* Shift the pattern so that the next
-character in text aligns with the last
-occurrence of it in pattern.
-The condition s+m < n is necessary for
-the case when pattern occurs at the end
-of text */
-//txt[s+m] is character after the pattern in text
-                s += (s+m < n)? m-badchar[txt[s+m]] : 1;
+                System.out.println("pattern found at at shift s =:"+shift);
+                shift+=(shift+m<n)?m-bad_chars[text.charAt(shift+m)]:1;
+
             }
-            else
-/* Shift the pattern so that the bad character
-in text aligns with the last occurrence of
-it in pattern. The max function is used to
-make sure that we get a positive shift.
-We may get a negative shift if the last
-occurrence of bad character in pattern
-is on the right side of the current
-character. */
-                s += max(1, j - badchar[txt[s+j]]);
+            else shift+=Math.max( j-bad_chars[text.charAt(shift+j)],1 );
+
+
         }
-    }
-    /* Driver program to test above function */
-    public static void main(String []args) {
-        char txt[] = "ABAAABCD".toCharArray();
-        char pat[] = "ABC".toCharArray();
-        search(txt, pat);
+
     }
 
+    private static void bad_huristic(String pat, int m, int[] bad_chars) {
+        for (int i = 0; i <m ; i++) {
+            bad_chars[i]=-1;
+
+        }
+        for (int i = 0; i <m ; i++) {
+            bad_chars[(int)pat.charAt(i)]=i;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String text="ABAABCAABCD";
+        String pat="ABC";
+        patternSearch((text), (pat));
+
+    }
 }
